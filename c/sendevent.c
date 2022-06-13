08e93990e94d92a3c4b0de4ad2a9f1e603761544
Ahmad Malik <arma7x@live.com>
2022-06-13 20:03:32 +0800

    DONE: alphanumeric

--- c/sendevent.c
+++ c/sendevent.c
@@ -9,6 +9,13 @@ void send_event(char* evt, int key) {
     // printf("%s\n\r", command);
 }
 
+void send_event_alphanumeric(char* evt, int key, int repeat) {
+    for (int i=1;i<=repeat;i++) {
+        send_event(evt, key);
+    }
+    send_event("dev/input/event0", 106);
+}
+
 int main() {
     int input;
 
@@ -50,6 +57,117 @@ int main() {
                 break;
             case 9: // Call => Tab
                 send_event("dev/input/event0", 231);
+                break;
+            case 32: // Key_0 space
+                send_event_alphanumeric("dev/input/event0", 11, 1);
+                break;
+            case 48: // Key_0 0
+                send_event_alphanumeric("dev/input/event0", 11, 2);
+                break;
+            case 49: // Key_1 0
+                send_event_alphanumeric("dev/input/event0", 2, 5);
+                break;
+            case 97: // Key_2 a
+                send_event_alphanumeric("dev/input/event0", 3, 1);
+                break;
+            case 98: // Key_2 b
+                send_event_alphanumeric("dev/input/event0", 3, 2);
+                break;
+            case 99: // Key_2 c
+                send_event_alphanumeric("dev/input/event0", 3, 3);
+                break;
+            case 50: // Key_2 2
+                send_event_alphanumeric("dev/input/event0", 3, 4);
+                break;
+            case 100: // Key_3 d
+                send_event_alphanumeric("dev/input/event0", 4, 1);
+                break;
+            case 101: // Key_3 e
+                send_event_alphanumeric("dev/input/event0", 4, 2);
+                break;
+            case 102: // Key_3 f
+                send_event_alphanumeric("dev/input/event0", 4, 3);
+                break;
+            case 51: // Key_3 3
+                send_event_alphanumeric("dev/input/event0", 4, 4);
+                break;
+            case 103: // Key_4 g
+                send_event_alphanumeric("dev/input/event0", 5, 1);
+                break;
+            case 104: // Key_4 h
+                send_event_alphanumeric("dev/input/event0", 5, 2);
+                break;
+            case 105: // Key_4 i
+                send_event_alphanumeric("dev/input/event0", 5, 3);
+                break;
+            case 52: // Key_4 4
+                send_event_alphanumeric("dev/input/event0", 5, 4);
+                break;
+            case 106: // Key_5 j
+                send_event_alphanumeric("dev/input/event0", 6, 1);
+                break;
+            case 107: // Key_5 k
+                send_event_alphanumeric("dev/input/event0", 6, 2);
+                break;
+            case 108: // Key_5 l
+                send_event_alphanumeric("dev/input/event0", 6, 3);
+                break;
+            case 53: // Key_5 5
+                send_event_alphanumeric("dev/input/event0", 6, 4);
+                break;
+            case 109: // Key_6 m
+                send_event_alphanumeric("dev/input/event0", 7, 1);
+                break;
+            case 110: // Key_6 n
+                send_event_alphanumeric("dev/input/event0", 7, 2);
+                break;
+            case 111: // Key_6 0
+                send_event_alphanumeric("dev/input/event0", 7, 3);
+                break;
+            case 54: // Key_6 6
+                send_event_alphanumeric("dev/input/event0", 7, 4);
+                break;
+            case 112: // Key_7 p
+                send_event_alphanumeric("dev/input/event0", 8, 1);
+                break;
+            case 113: // Key_7 q
+                send_event_alphanumeric("dev/input/event0", 8, 2);
+                break;
+            case 114: // Key_7 r
+                send_event_alphanumeric("dev/input/event0", 8, 3);
+                break;
+            case 115: // Key_7 s
+                send_event_alphanumeric("dev/input/event0", 8, 4);
+                break;
+            case 55: // Key_7 7
+                send_event_alphanumeric("dev/input/event0", 8, 5);
+                break;
+            case 116: // Key_8 t
+                send_event_alphanumeric("dev/input/event0", 9, 1);
+                break;
+            case 117: // Key_8 u
+                send_event_alphanumeric("dev/input/event0", 9, 2);
+                break;
+            case 118: // Key_8 v
+                send_event_alphanumeric("dev/input/event0", 9, 3);
+                break;
+            case 56: // Key_8 8
+                send_event_alphanumeric("dev/input/event0", 9, 4);
+                break;
+            case 119: // Key_9 w
+                send_event_alphanumeric("dev/input/event0", 10, 1);
+                break;
+            case 120: // Key_9 x
+                send_event_alphanumeric("dev/input/event0", 10, 2);
+                break;
+            case 121: // Key_9 y
+                send_event_alphanumeric("dev/input/event0", 10, 3);
+                break;
+            case 122: // Key_9 z
+                send_event_alphanumeric("dev/input/event0", 10, 4);
+                break;
+            case 57: // Key_9 9
+                send_event_alphanumeric("dev/input/event0", 10, 5);
                 break;
         }
         // printf("\33[2K\r");
